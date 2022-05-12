f174cf55f1122625fc1f641f53504d1416563d52
Alejandro Hernandez <werunthenite@gmail.com>
2022-05-12 18:32:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { Table } from 'reactstrap';
+import axios from 'axios';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 function App() {
+
+  const [personas, setPersonas] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://${window.location.hostname}:8080/v1/persona/all`)
+    .then(function (response) {
+      setPersonas(response.data);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='content'>
+    <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nombre</th>
+          <th>Edad</th>
+          <th>Timestamp</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          personas.map((persona, i) => (
+            <tr key={i}>
+              <th scope="row">{persona.personaID}</th>
+              <td>{persona.nombre}</td>
+              <td>{persona.edad}</td>
+              <td>{persona.timestamp}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </Table>
     </div>
   );
 }
